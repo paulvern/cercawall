420ddc6f3bb25486c46e6de95e7ecdd4fe3118c7
paulvern <paulvern@gmail.com>
2025-03-19 12:13:13 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "wv-scraper",
+  "version": "1.0.0",
+  "main": "server.js",
+  "dependencies": {
+    "express": "^4.18.2",
+    "puppeteer": "^21.3.1"
+  },
+  "scripts": {
+    "start": "node server.js"
+  }
+}
